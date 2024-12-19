7a1f31929d96b99006024205b5bdc2431187415d
Raí <devvandenberg>
2024-12-18 21:04:00 -0300

    funcao de adicionar e subtrair

---
+++ js/funcoes.js
@@ -0,0 +1,20 @@
+function adicionar(produto){
+    const qtd= document.getElementById('qtd_' + produto)
+    const valor= document.getElementById('valor_' + produto)
+    const total= document.getElementById('total_' + produto)
+    
+    qtd.innerHTML++
+   total.innerHTML = qtd.innerHTML * valor.innerHTML
+
+   
+}
+
+function subtrair(produto){
+    const qtd= document.getElementById('qtd_' + produto)
+    const valor= document.getElementById('valor_' + produto)
+    const total= document.getElementById('total_' + produto)
+    
+    qtd.innerHTML--
+    total.innerHTML = qtd.innerHTML * valor.innerHTML
+}
+
