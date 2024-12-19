5a439d2e37b02cccbf7df1e586fdea5f39b7e86d
Raí <devvandenberg>
2024-12-18 21:49:41 -0300

    finalizando o carrinho de compras

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,20 +1,35 @@
-function adicionar(produto){
+
+function alterarQtd(produto, acao){
     const qtd= document.getElementById('qtd_' + produto)
     const valor= document.getElementById('valor_' + produto)
     const total= document.getElementById('total_' + produto)
-    
-    qtd.innerHTML++
-   total.innerHTML = qtd.innerHTML * valor.innerHTML
 
-   
+    if(acao == '-' && qtd.innerHTML ==0){
+        alert('Atenção! A QUANTIDADE NÃO PODE SER MENOR QUE 0')
+    } else{
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+
+    const valorTotal = qtd.innerHTML * somenteNumeros(valor.innerHTML)
+    total.innerHTML = formatarValor(valorTotal)
+    soma()
+    }
+    
+    
 }
 
-function subtrair(produto){
-    const qtd= document.getElementById('qtd_' + produto)
-    const valor= document.getElementById('valor_' + produto)
-    const total= document.getElementById('total_' + produto)
+function soma() {
+    let soma = 0
+    for(let i = 1; i < 4; i++){
+        let numero = somenteNumeros (document.getElementById('total_' + i).innerHTML)
+        soma += Number(numero)
+    }
     
-    qtd.innerHTML--
-    total.innerHTML = qtd.innerHTML * valor.innerHTML
+    document.getElementById('subtotal').innerHTML = formatarValor(soma)
 }
 
+function somenteNumeros(n){
+    return n.replace(/\D/g, '')
+}
+function formatarValor(n){
+    return 'R$ ' + n.toLocaleString('pt-BR')
+}
